b42175c0851650dafd1891d21819d9bfe2711b83
Sean Haneberg <haneberg@gmail.com>
2024-11-28 00:57:53 -0800

    feat: nvim dap debugger configuration

--- init.lua
+++ init.lua
@@ -1,12 +1,12 @@
 vim.opt.clipboard = "unnamedplus"
 
 vim.opt.listchars = {
-    tab = '▸▸',
-    extends = 'E',
-    precedes = 'P',
-    space = '∙',
-    trail = '◦',
-    eol = '┤'
+  tab = '▸▸',
+  extends = 'E',
+  precedes = 'P',
+  space = '∙',
+  trail = '◦',
+  eol = '┤'
 }
 
 -- Tabs
@@ -32,25 +32,25 @@ vim.opt.shada = "'100,<50,s10,h"
 
 -- Open URL or path under cursor
 local function open_path()
-    local path = vim.fn.expand(vim.fn.expand("<cWORD>"))
-    local open_cmd
+  local path = vim.fn.expand(vim.fn.expand("<cWORD>"))
+  local open_cmd
 
-    if vim.fn.has("mac") == 1 then
-        open_cmd = "open"
-    elseif vim.fn.has("unix") == 1 then
-        open_cmd = "xdg-open"
-    elseif vim.fn.has("win32") == 1 then
-        open_cmd = "start"
-    else
-        print("Unsupported OS")
-        return
-    end
+  if vim.fn.has("mac") == 1 then
+    open_cmd = "open"
+  elseif vim.fn.has("unix") == 1 then
+    open_cmd = "xdg-open"
+  elseif vim.fn.has("win32") == 1 then
+    open_cmd = "start"
+  else
+    print("Unsupported OS")
+    return
+  end
 
-    if open_cmd == "start" then
-        vim.fn.jobstart({"cmd.exe", "/C", open_cmd, path}, {detach = true})
-    else
-        vim.fn.jobstart({open_cmd, path}, {detach = true})
-    end
+  if open_cmd == "start" then
+    vim.fn.jobstart({"cmd.exe", "/C", open_cmd, path}, {detach = true})
+  else
+    vim.fn.jobstart({open_cmd, path}, {detach = true})
+  end
 end
 
 vim.api.nvim_create_user_command('OpenUrl', open_path, {})
@@ -58,9 +58,9 @@ vim.api.nvim_set_keymap('n', '<leader>o', ':OpenUrl<CR>', { noremap = true, sile
 
 -- Trim trailing whitespace
 local function trim_trailing_whitespace()
-    local view = vim.fn.winsaveview()
-    vim.cmd([[keeppatterns %s/\s\+$//e]])
-    vim.fn.winrestview(view)
+  local view = vim.fn.winsaveview()
+  vim.cmd([[keeppatterns %s/\s\+$//e]])
+  vim.fn.winrestview(view)
 end
 
 vim.api.nvim_create_user_command('TrimWhitespace', trim_trailing_whitespace, {})
@@ -77,93 +77,232 @@ vim.api.nvim_set_keymap('n', 'Y', 'yy', { noremap = true, silent = true })
 
 -- Neovide-specific settings
 if vim.g.neovide then
-    vim.g.neovide_cursor_vfx_mode = "wireframe"
-    vim.api.nvim_set_keymap('n', '<F11>', ":lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>", { noremap = true, silent = true })
+  vim.g.neovide_cursor_vfx_mode = "wireframe"
+  vim.api.nvim_set_keymap('n', '<F11>', ":lua vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen<CR>", { noremap = true, silent = true })
 end
 
 -- GUI-specific settings
 if vim.fn.has("gui_running") == 1 then
-    function adjust_font_size(delta)
-        local guifont = table.concat(vim.opt.guifont:get(), ",")
-        local size = guifont:match('h(%d+)')
-        if size then
-            size = tonumber(size) + delta
-            vim.opt.guifont = guifont:gsub('h%d+', 'h' .. size)
-        end
+  function adjust_font_size(delta)
+    local guifont = table.concat(vim.opt.guifont:get(), ",")
+    local size = guifont:match('h(%d+)')
+    if size then
+      size = tonumber(size) + delta
+      vim.opt.guifont = guifont:gsub('h%d+', 'h' .. size)
     end
+  end
 
-    vim.api.nvim_set_keymap('n', '<C-=>', ":lua adjust_font_size(1)<CR>", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap('n', '<C-->', ":lua adjust_font_size(-1)<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<C-=>', ":lua adjust_font_size(1)<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<C-->', ":lua adjust_font_size(-1)<CR>", { noremap = true, silent = true })
 
-    vim.opt.guifont = "Cascadia Code:h14:cANSI"
+  vim.opt.guifont = "Cascadia Code:h14:cANSI"
 
-    vim.opt.guicursor = {
-        "n-v-c:block-Cursor",                  -- Normal, Visual, Command-line: block
-        "i-ci-ve:ver25-Cursor",                -- Insert, Command-line Insert, Visual: vertical bar cursor 25% width
-        "r-cr:hor20-Cursor",                   -- Replace, Command-line Replace: horizontal bar cursor 20% height
-        "o:hor50",                             -- Operator-pending: horizontal bar cursor 50% height
-        "a:blinkwait700-blinkoff400-blinkon250-Cursor"
-    }
+  vim.opt.guicursor = {
+    "n-v-c:block-Cursor",                  -- Normal, Visual, Command-line: block
+    "i-ci-ve:ver25-Cursor",                -- Insert, Command-line Insert, Visual: vertical bar cursor 25% width
+    "r-cr:hor20-Cursor",                   -- Replace, Command-line Replace: horizontal bar cursor 20% height
+    "o:hor50",                             -- Operator-pending: horizontal bar cursor 50% height
+    "a:blinkwait700-blinkoff400-blinkon250-Cursor"
+  }
 end
 
 -- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "nvim-lua/plenary.nvim" },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+  { "nvim-lua/plenary.nvim" },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
 
-            -- ts_ls setup for TypeScript/JavaScript
-            lspconfig.ts_ls.setup({
-                on_attach = function(client, bufnr)
-                    -- Disable ts_ls formatting (use Prettier/null-ls instead)
-                    client.server_capabilities.documentFormattingProvider = false
+      -- ts_ls setup for TypeScript/JavaScript
+      lspconfig.ts_ls.setup({
+        on_attach = function(client, bufnr)
+          -- Disable ts_ls formatting (use Prettier/null-ls instead)
+          client.server_capabilities.documentFormattingProvider = false
 
-                    -- Key mappings for LSP functionality
-                    local opts = { noremap = true, silent = true, buffer = bufnr }
-                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-                end,
-                flags = { debounce_text_changes = 150 },
-            })
+          -- Key mappings for LSP functionality
+          local opts = { noremap = true, silent = true, buffer = bufnr }
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         end,
+        flags = { debounce_text_changes = 150 },
+      })
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", ".git/" },
+        }
+      })
+      vim.api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      -- Optional UI for nvim-dap
+      "rcarriga/nvim-dap-ui",
+      -- JavaScript/TypeScript adapter for nvim-dap
+      "mxsdev/nvim-dap-vscode-js",
+      -- Telescope integration
+      "nvim-telescope/telescope-dap.nvim",
+      -- Dependency for dap-ui
+      "nvim-neotest/nvim-nio",
     },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").setup({
-                defaults = {
-                    file_ignore_patterns = { "node_modules", ".git/" },
-                }
-            })
-            vim.api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
-        end,
-    },
-    {
-        "cocopon/iceberg.vim",
-        config = function()
-            vim.cmd.colorscheme("iceberg")
-        end,
-    }
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      local layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.3 },
+            { id = "breakpoints", size = 0.2 },
+            { id = "stacks", size = 0.3 },
+            { id = "watches", size = 0.2 },
+          },
+          size = 0.3,
+          position = "left", -- Sidebar
+        },
+        {
+          elements = {
+            { id = "repl", size = 1.0 }, -- REPL
+            -- { id = "console", size = 0.5 }, -- Debug Console
+          },
+          size = 0.25,
+          position = "bottom", -- Bottom panel
+        },
+      }
+
+      vim.api.nvim_set_keymap("n", "<leader>do", ":lua require('dapui').open()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>dO", ":lua require('dapui').close()<CR>", { noremap = true, silent = true })
+      -- Configure dap-ui
+      dapui.setup({layouts = layouts})
+
+      -- Automatically open/close the UI during debugging
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- Set up JavaScript/TypeScript debugging (nvim-dap-vscode-js)
+      local dap_vscode_js = require("dap-vscode-js")
+      dap_vscode_js.setup({
+        debugger_path = vim.fn.stdpath("data") .. "/vscode-js-debug",
+        adapters = { "pwa-node", "pwa-chrome" },
+      })
+
+      -- Define DAP configurations for JavaScript and TypeScript
+      for _, language in ipairs({ "javascript", "typescript", "javascriptreact", "typescriptreact" }) do
+        dap.configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch Node.js Program",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-chrome",
+            request = "launch",
+            name = "Launch Chrome",
+            url = "http://localhost:3000", -- Adjust as needed
+            webRoot = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach to Node.js",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+
+      -- Keybindings for Debugging
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap("n", "<F5>", ":lua require'dap'.continue()<CR>", opts) -- Start/Continue
+      vim.api.nvim_set_keymap("n", "<F10>", ":lua require'dap'.step_over()<CR>", opts) -- Step Over
+      vim.api.nvim_set_keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts) -- Step Into
+      vim.api.nvim_set_keymap("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts) -- Step Out
+      vim.api.nvim_set_keymap("n", "<leader>dd", ":lua require('dap').terminate()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>dq", ":lua require('dap').disconnect()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>du", ":lua require('dapui').toggle()<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", opts) -- Toggle Breakpoint
+      vim.api.nvim_set_keymap("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts) -- Conditional Breakpoint
+      vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>", opts) -- Open REPL
+
+      -- Telescope Integration for DAP
+      require("telescope").load_extension("dap")
+      vim.api.nvim_set_keymap("n", "<leader>dc", ":Telescope dap configurations<CR>", opts) -- Debug Configurations
+      vim.api.nvim_set_keymap("n", "<leader>dv", ":Telescope dap variables<CR>", opts) -- Debug Variables
+      vim.api.nvim_set_keymap("n", "<leader>df", ":Telescope dap frames<CR>", opts) -- Debug Frames
+      vim.api.nvim_set_keymap("n", "<leader>dl", ":Telescope dap list_breakpoints<CR>", opts) -- List Breakpoints
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("nvim-web-devicons").setup()
+    end
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+      require("nvim-tree").setup({
+        update_focused_file = {
+          enable = true
+        }
+      })
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {"python", "lua", "c", "c_sharp", "javascript", "java", "markdown", "typescript"},
+        highlight = {
+          enable = true
+        },
+        indent = {
+          enable = true
+        },
+      })
+    end
+  },
+  {
+    "cocopon/iceberg.vim",
+    config = function()
+      vim.cmd.colorscheme("iceberg")
+    end,
+  }
 })
