10b9da9b2571d630c1ad4c5630a093a7b5f8a233
Sean Haneberg <haneberg@gmail.com>
2024-11-27 16:14:08 -0800

    feat: react & typescript support in init.lua

--- init.lua
+++ init.lua
@@ -10,9 +10,9 @@ vim.opt.listchars = {
 }
 
 -- Tabs
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = false
 
@@ -20,8 +20,8 @@ vim.opt.incsearch = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.colorcolumn = '121'
-vim.opt.textwidth=120
+vim.opt.colorcolumn = '81'
+vim.opt.textwidth = 80
 
 vim.opt.number = true
 vim.o.list = true
@@ -32,7 +32,7 @@ vim.opt.shada = "'100,<50,s10,h"
 
 -- Open URL or path under cursor
 local function open_path()
-    local path = vim.fn.expand(vim.fn.expand("<cWORD>")) -- Exapnd twice to parse paths with ~
+    local path = vim.fn.expand(vim.fn.expand("<cWORD>"))
     local open_cmd
 
     if vim.fn.has("mac") == 1 then
@@ -47,7 +47,6 @@ local function open_path()
     end
 
     if open_cmd == "start" then
-        -- Blech!
         vim.fn.jobstart({"cmd.exe", "/C", open_cmd, path}, {detach = true})
     else
         vim.fn.jobstart({open_cmd, path}, {detach = true})
@@ -57,17 +56,17 @@ end
 vim.api.nvim_create_user_command('OpenUrl', open_path, {})
 vim.api.nvim_set_keymap('n', '<leader>o', ':OpenUrl<CR>', { noremap = true, silent = true })
 
--- Define a function to trim trailing whitespace
+-- Trim trailing whitespace
 local function trim_trailing_whitespace()
     local view = vim.fn.winsaveview()
-    vim.cmd([[keeppatterns %s/\s\+$//e]]) -- Keep existing highlighting with keeppatterns.
+    vim.cmd([[keeppatterns %s/\s\+$//e]])
     vim.fn.winrestview(view)
 end
 
 vim.api.nvim_create_user_command('TrimWhitespace', trim_trailing_whitespace, {})
 vim.api.nvim_set_keymap('n', '<leader>t', ':TrimWhitespace<CR>', { noremap = true, silent = true })
 
--- Map <leader>y to copy full path of current file to clipboard
+-- Copy file path to clipboard
 vim.api.nvim_set_keymap('n', '<leader>y', ':let @+ = expand("%:p")<CR>', { noremap = true, silent = true })
 
 -- Remove Highlights
@@ -107,7 +106,7 @@ if vim.fn.has("gui_running") == 1 then
     }
 end
 
--- Lazy bootstrap
+-- Lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
@@ -124,55 +123,47 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     { "nvim-lua/plenary.nvim" },
     {
-        "nvim-telescope/telescope.nvim",
+        "neovim/nvim-lspconfig",
         config = function()
-            vim.api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>fb', ":Telescope buffers<CR>", { noremap = true, silent = true })
-            vim.api.nvim_set_keymap('n', '<leader>fh', ":Telescope help_tags<CR>", { noremap = true, silent = true })
+            local lspconfig = require("lspconfig")
 
-            require('telescope').setup()
-        end
+            -- ts_ls setup for TypeScript/JavaScript
+            lspconfig.ts_ls.setup({
+                on_attach = function(client, bufnr)
+                    -- Disable ts_ls formatting (use Prettier/null-ls instead)
+                    client.server_capabilities.documentFormattingProvider = false
+
+                    -- Key mappings for LSP functionality
+                    local opts = { noremap = true, silent = true, buffer = bufnr }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                end,
+                flags = { debounce_text_changes = 150 },
+            })
+        end,
     },
     {
-        "nvim-tree/nvim-web-devicons",
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
-            require("nvim-web-devicons").setup()
-        end
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        config = function()
-            vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-            require("nvim-tree").setup({
-                update_focused_file = {
-                    enable = true
+            require("telescope").setup({
+                defaults = {
+                    file_ignore_patterns = { "node_modules", ".git/" },
                 }
             })
-        end
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = {"python", "lua", "c", "c_sharp", "javascript", "java", "markdown"},
-                highlight = {
-                    enable = true
-                },
-                indent = {
-                    enable = true
-                },
-            })
-        end
+            vim.api.nvim_set_keymap('n', '<leader>ff', ":Telescope find_files<CR>", { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>fg', ":Telescope live_grep<CR>", { noremap = true, silent = true })
+        end,
     },
     {
         "cocopon/iceberg.vim",
         config = function()
             vim.cmd.colorscheme("iceberg")
-        end
+        end,
     }
 })
-
-vim.cmd.filetype("indent off")
-vim.cmd.filetype("plugin on")
-vim.cmd.syntax("enable")
